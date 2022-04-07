c8c6d7dc770b0f26d207857b9f616f6181be2615
Thomas Propson <tcpropson@protonmail.com>
2022-04-07 11:10:40 -0400

    sudo apt update

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# update apt
+sudo apt update
+
 # emacs
-sudo apt-get install emacs
+sudo apt install emacs
 cp ./emacs/.emacs ~/.emacs
 # TODO: M-x package-refresh-contents; M-x package-install julia-mode
 
 # tmux
-sudo apt-get install tmux
+sudo apt install tmux
 cp ./tmux/.tmux.conf ~/.tmux.conf
 
